221a22c8863946aaab3a27ab40a4861b873cc6c5
Andrey Mashenko <inexacts@gmail.com>
2010-05-22 14:13:22 +0000

    logic-cache branch: adding ehcache config. adding listeners of cache and cacheManager.

---
+++ src/api/org/openmrs/logic/cache/LogicCacheEventListener.java
@@ -0,0 +1,62 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.logic.cache;
+
+import net.sf.ehcache.CacheException;
+import net.sf.ehcache.Ehcache;
+import net.sf.ehcache.Element;
+import net.sf.ehcache.event.CacheEventListener;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+
+public class LogicCacheEventListener implements CacheEventListener {
+
+    private static Log log = LogFactory.getLog(LogicCacheEventListener.class);
+
+    @Override
+    public void notifyElementRemoved(Ehcache ehcache, Element element) throws CacheException {
+        log.info("Cache ElementRemoved");
+    }
+
+    @Override
+    public void notifyElementPut(Ehcache ehcache, Element element) throws CacheException {
+        log.info("Cache ElementPut");
+    }
+
+    @Override
+    public void notifyElementUpdated(Ehcache ehcache, Element element) throws CacheException {
+        log.info("Cache ElementUpdated");
+    }
+
+    @Override
+    public void notifyElementExpired(Ehcache ehcache, Element element) {
+        log.info("Cache ElementExpire");
+    }
+
+    @Override
+    public void notifyElementEvicted(Ehcache ehcache, Element element) {
+        log.info("Cache ElementEvicted");
+    }
+
+    @Override
+    public void notifyRemoveAll(Ehcache ehcache) {
+        log.info("Cache RemoveAll");
+    }
+
+    @Override
+    public void dispose() {
+        log.info("Cache disposed");
+    }
+}
---
+++ src/api/org/openmrs/logic/cache/LogicCacheManagerEventListener.java
@@ -0,0 +1,57 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.logic.cache;
+
+import net.sf.ehcache.CacheException;
+import net.sf.ehcache.CacheManager;
+import net.sf.ehcache.Status;
+import net.sf.ehcache.event.CacheManagerEventListener;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+
+/**
+ *
+ */
+public class LogicCacheManagerEventListener implements CacheManagerEventListener {
+	
+	private static Log log = LogFactory.getLog(LogicCacheManagerEventListener.class);
+
+	@Override
+    public void dispose() throws CacheException {
+	    log.info("Cache Manager disposed");
+    }
+
+	@Override
+    public Status getStatus() {
+	    return CacheManager.getInstance().getStatus();
+    }
+
+	@Override
+    public void init() throws CacheException {
+		log.info("Cache Manager init");	    
+    }
+
+	@Override
+    public void notifyCacheAdded(String arg0) {
+		log.info("Cache Manager CacheAdded");	    
+    }
+
+	@Override
+    public void notifyCacheRemoved(String arg0) {
+		log.info("Cache Manager CacheRemoved");	    
+    }
+
+}
