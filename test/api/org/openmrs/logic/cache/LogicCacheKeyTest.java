2e9ad58f80f04f8252c492d4cf03d027275c0c48
Andrey Mashenko <inexacts@gmail.com>
2010-06-12 07:44:25 +0000

    logic-cache branch: test Result for serialization/deserialization

--- test/api/org/openmrs/logic/cache/LogicCacheKeyTest.java
+++ test/api/org/openmrs/logic/cache/LogicCacheKeyTest.java
@@ -15,14 +15,20 @@ package org.openmrs.logic.cache;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.openmrs.Patient;
 import org.openmrs.api.context.Context;
 import org.openmrs.logic.LogicCriteria;
 import org.openmrs.logic.LogicService;
 import org.openmrs.logic.datasource.LogicDataSource;
+import org.openmrs.logic.result.Result;
+import org.openmrs.logic.util.LogicUtil;
 import org.openmrs.test.BaseModuleContextSensitiveTest;
 
 import java.io.*;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
@@ -35,21 +41,28 @@ public class LogicCacheKeyTest extends BaseModuleContextSensitiveTest {
 //    private LogicCache logicCache;
     private LogicCacheKey logicCacheKey1;
     private LogicCacheKey logicCacheKey2;
+    private LogicService logicService;
 
     @Before
     public void setUp() throws Exception {
 //        logicCache = LogicCacheManager.getDefaultLogicCache();
 //        assertNotNull("logicCache is null", logicCache);
 
-        LogicService logicService = Context.getLogicService();
+        logicService = Context.getLogicService();
+        assertNotNull(logicService);
+
         LogicCriteria lc1 = logicService.parse("\"AGE\"");
         LogicDataSource logicDataSource1 = logicService.getLogicDataSource("obs");
-        logicCacheKey1 = new LogicCacheKey(null, lc1, logicDataSource1, new Date(), 2);
+        Map<String, Object> parameters1 = new HashMap<String, Object>();
+        parameters1.put("1", 1);
+        logicCacheKey1 = new LogicCacheKey(parameters1, lc1, logicDataSource1, new Date(), 2);
         assertNotNull("logicCacheKey1 is NULL", logicCacheKey1);
 
         LogicCriteria lc2 = logicService.parse("\"AGE\"");
         LogicDataSource logicDataSource2 = logicService.getLogicDataSource("obs");
-        logicCacheKey2 = new LogicCacheKey(null, lc2, logicDataSource2, new Date(), 2);
+        Map<String, Object> parameters2 = new HashMap<String, Object>();
+        parameters2.put("1", 1);
+        logicCacheKey2 = new LogicCacheKey(parameters2, lc2, logicDataSource2, new Date(), 2);
         assertNotNull("logicCacheKey2 is NULL", logicCacheKey2);
     }
 
@@ -59,11 +72,27 @@ public class LogicCacheKeyTest extends BaseModuleContextSensitiveTest {
     }
 
     @Test
-    public void testRoundTripSerialization() throws IOException, ClassNotFoundException {
+    public void testCacheKeySerialization() throws IOException, ClassNotFoundException {
+        LogicCacheKey deserializedLogicCacheKey = (LogicCacheKey) roundTripSerialization(logicCacheKey1);
+
+        assertEquals("logicCacheKey1 and deserializedLogicCacheKey are not equals", logicCacheKey1, deserializedLogicCacheKey);
+    }
+
+    @Test
+    public void testResultSerialization() throws ClassNotFoundException, IOException {
+        LogicUtil.registerDefaultRules();
+        Patient patient = Context.getPatientService().getPatient(2);
+        Result result = logicService.eval(patient, logicService.parse("\"AGE\""));
+        Result deserializedResult = (Result) roundTripSerialization(result);
+
+        assertEquals("result and deserializedResult are not equals", result, deserializedResult);
+    }
+
+    private Object roundTripSerialization(Object forSerialization) throws IOException, ClassNotFoundException {
         //serialize
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         ObjectOutputStream oos = new ObjectOutputStream(out);
-        oos.writeObject(logicCacheKey1);
+        oos.writeObject(forSerialization);
         oos.close();
         assertTrue("Not serialized by stream.", out.toByteArray().length > 0);
 
@@ -71,10 +100,8 @@ public class LogicCacheKeyTest extends BaseModuleContextSensitiveTest {
         byte[] pickled = out.toByteArray();
         InputStream in = new ByteArrayInputStream(pickled);
         ObjectInputStream ois = new ObjectInputStream(in);
-        Object o = ois.readObject();
+        Object serializedObj = ois.readObject();
 
-        LogicCacheKey deserializedLogicCacheKey = (LogicCacheKey) o;
-
-        assertEquals("logicCacheKey1 and deserializedLogicCacheKey are not equals", logicCacheKey1, deserializedLogicCacheKey);
+        return serializedObj;
     }
 }
