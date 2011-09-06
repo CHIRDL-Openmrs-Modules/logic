d372053855044c4942d6f03f42ff05ec1233e756
Jazayeri, Darius <darius@openmrs.org>
2011-09-06 21:07:55 +0000

    indexDate needs to be inclusive - LOGIC-92 author: tmdugan

--- src/api/org/openmrs/logic/impl/LogicContextImpl.java
+++ src/api/org/openmrs/logic/impl/LogicContextImpl.java
@@ -134,10 +134,10 @@ public class LogicContextImpl implements LogicContext {
 	 */
 	private Date newIndexDate() {
 		Calendar cal = Calendar.getInstance();
-		cal.set(Calendar.HOUR_OF_DAY, 0);
-		cal.set(Calendar.MINUTE, 0);
-		cal.set(Calendar.SECOND, 0);
-		cal.set(Calendar.MILLISECOND, 0);
+		cal.set(Calendar.HOUR_OF_DAY, 23);
+		cal.set(Calendar.MINUTE, 59);
+		cal.set(Calendar.SECOND, 59);
+		cal.set(Calendar.MILLISECOND, 999);
 		return cal.getTime();
 	}
 	
